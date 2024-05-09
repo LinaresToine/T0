031042b902f062e7c557f884d98d3e184f7a7eb9
Antonio Linares Sancho <132582167+LinaresToine@users.noreply.github.com>
2024-05-09 11:18:26 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,129 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+"""
+Build, clean and test the WMCore package.
+"""
+from __future__ import print_function
 
-# Read requirements from requirements.txt file
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+import imp
+import os
+import os.path
+from distutils.core import Command, setup
+from os.path import join as pjoin
 
-setup(
-    name="T0",
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    description="This package contains the code that is involved in the Tier 0 agent when its deployed",
-    author="Dirk Hufnagel",
-    author_email="Dirk.Hufnagel@cern.ch",
-    maintainer="WMCore",
-    maintainer_email="ms.unmerged@cern.ch",
-    license="MIT",
-    packages=find_packages(),
-    package_dir={"": "src/python"},
-    package_data={
-        '': ['etc/*', 'bin/*'],
-    },
-    include_package_data=True,
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX",
-        "Programming Language :: Python",
-        "Topic :: Database",
-    ],
-    install_requires=requirements,  # Use requirements from requirements.txt
-    python_requires=">=3.8",
-    url="https://github.com/dmwm/T0",
-)
+from setup_build import BuildCommand, InstallCommand, get_path_to_wmcore_root, list_packages, list_static_files
+from setup_test import CoverageCommand, TestCommand
+
+
+class CleanCommand(Command):
+    description = "Clean up (delete) compiled files"
+    user_options = []
+
+    def initialize_options(self):
+        self.cleanMes = []
+        for root, dummyDirs, files in os.walk('.'):
+            for f in files:
+                if f.endswith('.pyc'):
+                    self.cleanMes.append(pjoin(root, f))
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        for cleanMe in self.cleanMes:
+            try:
+                os.unlink(cleanMe)
+            except Exception:
+                pass
+
+
+class EnvCommand(Command):
+    description = "Configure the PYTHONPATH, DATABASE and PATH variables to" + \
+                  "some sensible defaults, if not already set. Call with -q when eval-ing," + \
+                  """ e.g.:
+                      eval `python setup.py -q env`
+                  """
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        if not os.getenv('COUCHURL', False):
+            # Use the default localhost URL if none is configured.
+            print('export COUCHURL=http://localhost:5984')
+        here = get_path_to_wmcore_root()
+
+        tests = here + '/test/python'
+        source = here + '/src/python'
+        # Stuff we want on the path
+        exepth = [here + '/etc',
+                  here + '/bin']
+
+        pypath = os.getenv('PYTHONPATH', '').strip(':').split(':')
+
+        for pth in [tests, source]:
+            if pth not in pypath:
+                pypath.append(pth)
+
+        # We might want to add other executables to PATH
+        expath = os.getenv('PATH', '').split(':')
+        for pth in exepth:
+            if pth not in expath:
+                expath.append(pth)
+
+        print('export PYTHONPATH=%s' % ':'.join(pypath))
+        print('export PATH=%s' % ':'.join(expath))
+
+        # We want the WMCORE root set, too
+        print('export T0_ROOT=%s' % get_path_to_wmcore_root())
+        print('export T0BASE=$T0_ROOT')
+
+# The actual setup command, and the classes associated to the various options
+
+# Need all the packages we want to build by default, this will be overridden in sub-system builds.
+# Since it's a lot of code determine it by magic.
+DEFAULT_PACKAGES = list_packages(['src/python/T0',
+                                  'src/python/T0Component'
+                                 ])
+
+# Divine out the version of WMCore from WMCore.__init__, which is bumped by
+# "bin/buildrelease.sh"
+
+# Obnoxiously, there's a dependency cycle when building packages. We'd like
+# to simply get the current WMCore version by using
+# from WMCore import __version__
+# But PYTHONPATH isn't set until after the package is built, so we can't
+# depend on the python module resolution behavior to load the version.
+# Instead, we use the imp module to load the source file directly by
+# filename.
+t0_root = get_path_to_wmcore_root()
+t0_package = imp.load_source('temp_module', os.path.join(t0_root,
+                                                            'src',
+                                                            'python',
+                                                            'T0',
+                                                            '__init__.py'))
+t0_version = t0_package.__version__
+
+setup(name='T0',
+      version=t0_version,
+      maintainer='CMS DMWM Group',
+      maintainer_email='cms-tier0-operations@cern.ch',
+      cmdclass={'deep_clean': CleanCommand,
+                'coverage': CoverageCommand,
+                'test': TestCommand,
+                'env': EnvCommand,
+                'build_system': BuildCommand,
+                'install_system': InstallCommand},
+      # base directory for all our packages
+      package_dir={'': 'src/python/'},  # % get_path_to_wmcore_root()},
+      packages=DEFAULT_PACKAGES,
+      data_files=list_static_files(),
+      url="https://github.com/LinaresToine/T0",
+      license="Apache License, Version 2.0",
+      download_url = "https://github.com/LinaresToine/T0/tarball/%s" % t0_version
+
+      )
