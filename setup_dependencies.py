e1666fed35052763a87f0199c96bb69496187723
Antonio Linares Sancho <132582167+LinaresToine@users.noreply.github.com>
2024-05-13 16:07:07 -0500

    Update setup_dependencies.py

--- setup_dependencies.py
+++ setup_dependencies.py
@@ -6,6 +6,7 @@ dependencies = {
                      'T0Component+'],
         'modules': [],
         'systems': [],
-        'statics': []
+        'statics': ['bin+',
+                    'etc+']
     }
 }
